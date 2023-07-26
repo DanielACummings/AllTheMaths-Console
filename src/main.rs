a86d0e5f3a0369b799a408b82e8644c823681d64
Daniel <danielac@protonmail.com>
2023-07-25 18:26:32 -0600

    Simplify fibonacci algorithm & add beginning of user options

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+use std::io::stdin;
+
 fn main() {
-    let mut n1 = 0;
-    let mut n2 = 1;
+    let mut prev: i32 = 0;
+    let mut current: i32 = 1;
+    let user_options: [&str; 1] = ["Print 1st 10 fibonacci numbers"];
 
-    println!("1st 10 fibonacci numbers:");
-    // Print 1st 2 numbers
-    println!("{}\n{}", n1, n2);
+    // Print user_options as a numbered list
+    println!("Enter number below to choose option:");
+    for i in 0..user_options.len() {
+        println!("{}. {}", i + 1, user_options[i]);
+    }
 
-    // Calculate & print remaining numbers
-    for _ in 0..10 {
-        println!("{}", (n1 + n2));
+    // Get user input
+    let mut user_input: String = String::new();
+    stdin().read_line(&mut user_input).unwrap();
 
-        let temp = n2;
-        n2 = n1 + n2;
-        n1 = temp;
+    if user_input.trim() == "1" {
+        println!("\n1st 10 fibonacci numbers:");
+
+        for counter in 0..10 {
+            print!("{}:", (counter + 1).to_string());
+            println!("{}", prev);
+            let next: i32 = prev + current;
+            prev = current;
+            current = next;
+        }
     }
 }
