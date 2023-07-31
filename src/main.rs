eeaa6bcf7ac87b3fa69cb027f090ed5c3dd4d4b9
Daniel <danielac@protonmail.com>
2023-07-31 17:32:48 -0600

    Put options in loop with exit option

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,53 @@ use std::io::stdin;
 
 fn main() {
     // Todo: Turn user_options into HashMap
-    // Todo: Add while loop for getting user input which can be exited using a
-    // Exit option
-    let user_options: [&str; 2] = [
+    let user_options: [&str; 3] = [
         "Print 1st 10 fibonacci numbers",
         "Convert number to binary",
+        "Exit",
     ];
 
-    // Print user_options as a numbered list
-    println!("Enter number below to choose option:");
-    for i in 0..user_options.len() {
-        println!("{}. {}", i + 1, user_options[i]);
-    }
+    // Loop: display options & execute user choise until they exit 
+    loop {
+        // Print user_options as a numbered list
+        println!("Enter number below to choose option:");
+        for i in 0..user_options.len() {
+            println!("{}. {}", i + 1, user_options[i]);
+        }
+    
+        // Get user input
+        // Todo: Use Ok() or Err() for input validation
+        let mut user_option: String = String::new();
+        stdin().read_line(&mut user_option).unwrap();
+        user_option = user_option.trim().to_string();
 
-    // Get user input
-    // Todo: Use Ok() or Err() for input validation
-    let mut user_option: String = String::new();
-    stdin().read_line(&mut user_option).unwrap();
+        // Todo: reference HashMap in future
+        if user_option == "1" {
+            let fibonacci_text: String = fibonacci();
+            println!("{}", fibonacci_text);
+        } else if user_option == "2" {
+            println!("\nEnter number to convert to binary: ");
+            let mut user_num_res: String = String::new();
+            stdin().read_line(&mut user_num_res).unwrap();
+            let user_num: i32 = match user_num_res.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please enter a valid number");
+                    return;
+                }
+            };
+            
+            let u_user_num: u32 = user_num.try_into().unwrap();
+            println!(
+                "{} in binary: {}",
+                user_num,
+                number_to_binary(u_user_num)
+            );
+        } else if user_option == "3" {
+            break;
+        }
 
-    // Todo: reference HashMap in future
-    if user_option.trim() == "1" {
-        let fibonacci_text: String = fibonacci();
-        println!("{}", fibonacci_text);
-    } else if user_option.trim() == "2" {
-        println!("\nEnter number to convert to binary: ");
-        let mut user_num_res: String = String::new();
-        stdin().read_line(&mut user_num_res).unwrap();
-        let user_num: i32 = match user_num_res.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number");
-                return;
-            }
-        };
-        
-        let u_user_num: u32 = user_num.try_into().unwrap();
-        println!(
-            "{} in binary: {}",
-            user_num,
-            number_to_binary(u_user_num)
-        );
+        println!();
     }
 }
 
